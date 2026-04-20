52a2770de14ae150c9ecbe47be7c852310227446
Siva Sagar <sagar.sagarp1233@gmail.com>
2026-04-20 21:37:13 +0530

    Your message about today's changes

--- api/robots.txt.js
+++ api/robots.txt.js
@@ -35,7 +35,7 @@ Disallow: /vendors
 Disallow: /inspiration
 Disallow: /settings
 
-Sitemap: ${baseUrl}sitemap.xml
+Sitemap: https://wedora.in/sitemap.xml
 `;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.setHeader('Cache-Control', 'public, max-age=300, s-maxage=300');
